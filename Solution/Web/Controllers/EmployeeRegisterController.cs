1d77f0aaa69fa99b041f80ee4416436fdb5deef7
Caio Mendonça <kaito.mendonca@gmail.com>
2017-12-31 09:53:21 -0200

    Included fontawesome component and make adjustments for employee list view

--- Solution/Web/Controllers/EmployeeRegisterController.cs
+++ Solution/Web/Controllers/EmployeeRegisterController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace Web.Controllers
 {
-    public class EmployeeController : Controller
+    public class EmployeeRegisterController : Controller
     {
         public ActionResult Index()
         {
